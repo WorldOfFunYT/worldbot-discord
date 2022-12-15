e9d794f9db0d710983716d6d519075c4060f5635
WorldOfFunYT <czarczynskiszymon@gmail.com>
2022-12-15 16:22:02 +0000

    removed "node:" prefix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const path = require("node:path");
+const path = require("path");
 const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
 const dotenv = require("dotenv");
 dotenv.config();
