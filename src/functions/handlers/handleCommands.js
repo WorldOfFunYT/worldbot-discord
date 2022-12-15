5874bf7c904dd02ca997b7bfca70598436a8ce35
WorldOfFunYT <czarczynskiszymon@gmail.com>
2022-12-15 15:24:23 +0000

    Made global command deployment easier

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,60 +1,73 @@
-const {
-    REST,
-    Routes
-} = require('discord.js');
+const { REST, Routes } = require("discord.js");
 
-const fs = require('node:fs');
+const fs = require("node:fs");
 
-global = false
+global = process.env.IS_GLOBAL;
 
 module.exports = (client) => {
-    client.handleCommands = async () => {
-        const commandFolders = fs.readdirSync('./src/commands')
+    client.handleCommands = async() => {
+        const commandFolders = fs.readdirSync("./src/commands");
         client.commandArray = [];
         for (folder of commandFolders) {
-            const commandFiles = fs.readdirSync(`${__dirname}\\..\\..\\commands\\${folder}`).filter(file => file.endsWith('.js'));
+            const commandFiles = fs
+                .readdirSync(`${__dirname}\\..\\..\\commands\\${folder}`)
+                .filter((file) => file.endsWith(".js"));
 
             for (const file of commandFiles) {
                 const command = require(`${__dirname}\\..\\..\\commands\\${folder}/${file}`);
                 // Set a new item in the Collection with the key as the command name and the value as the exported module
-                if ('data' in command && 'execute' in command) {
+                if ("data" in command && "execute" in command) {
                     client.commands.set(command.data.name, command);
                     client.commandArray.push(command.data.toJSON());
                 } else {
-                    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+                    console.log(
+                        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+                    );
                 }
             }
         }
         // Construct and prepare an instance of the REST module
         const rest = new REST({
-            version: '10'
+            version: "10",
         }).setToken(process.env.TOKEN);
 
         // and deploy your commands!
-        (async () => {
+        (async() => {
             try {
-                console.log(`Started refreshing ${client.commandArray.length} application (/) commands.`);
+                console.log(
+                    `Started refreshing ${client.commandArray.length} application (/) commands.`
+                );
 
                 // The put method is used to fully refresh all commands in the guild with the current set
                 let data;
-                if (!global) {
-                    data = await rest.put(
-                        Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
-                            body: client.commandArray
-                        },
-                    );
-                } else if (global) {
-                    data = await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
-                        body: client.commandArray
-                    })
+                if (global != "true") {
+                    console.log("Refreshing test guild (/) commands");
+                    data = await rest
+                        .put(
+                            Routes.applicationGuildCommands(
+                                process.env.CLIENT_ID,
+                                process.env.GUILD_ID
+                            ), {
+                                body: client.commandArray,
+                            }
+                        )
+                        .catch(console.error);
+                } else if (global == "true") {
+                    console.log("Refreshing application (/) commands");
+                    data = await rest
+                        .put(Routes.applicationCommands(process.env.CLIENT_ID), {
+                            body: client.commandArray,
+                        })
+                        .catch(console.error);
                 }
 
-                console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+                console.log(
+                    `Successfully reloaded ${data.length} application (/) commands.`
+                );
             } catch (error) {
                 // And of course, make sure you catch and log any errors!
                 console.error(error);
             }
         })();
-
-    }
-}
+    };
+};
--- src/index.js
+++ src/index.js
@@ -1,41 +1,31 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const {
-    Client,
-    Events,
-    GatewayIntentBits,
-    Collection
-} = require('discord.js');
-const dotenv = require('dotenv');
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+const dotenv = require("dotenv");
 dotenv.config();
-const {
-    TOKEN,
-    SERVER_ID,
-    DATABASE_TOKEN
-} = process.env;
-const {
-    connect
-} = require('mongoose');
-
+const { TOKEN, SERVER_ID, DATABASE_TOKEN } = process.env;
+const { connect } = require("mongoose");
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+    intents: [GatewayIntentBits.Guilds],
 });
 
 client.commands = new Collection();
-client.embedColor = '0xf2338d'
+client.embedColor = "0xf2338d";
 
-const functionPath = path.join(__dirname, 'functions')
+const functionPath = path.join(__dirname, "functions");
 const functionFolders = fs.readdirSync(functionPath);
 for (const folder of functionFolders) {
-    const functionFiles = fs.readdirSync(`${functionPath}\\${folder}`).filter(file => file.endsWith('.js'));
+    const functionFiles = fs
+        .readdirSync(`${functionPath}\\${folder}`)
+        .filter((file) => file.endsWith(".js"));
     for (const file of functionFiles) {
         require(`${functionPath}/${folder}/${file}`)(client);
     }
 }
-client.handleEvents()
-client.handleCommands()
+client.handleEvents();
+client.handleCommands();
 client.login(TOKEN);
-(async () => {
-    await connect(DATABASE_TOKEN).catch(console.error)
-})()
+(async() => {
+    await connect(DATABASE_TOKEN).catch(console.error);
+})();
